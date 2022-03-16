b9c5b59d1eafa833986dc30d580b6df5c6c683dc
Max Pyle <maxpyle0@gmail.com>
2022-03-16 15:11:39 +1000

    Make rock paper scissors interactive via use of buttons

--- JS/rockpaperscissors.js
+++ JS/rockpaperscissors.js
@@ -1,4 +1,39 @@
+let wins = 0;
+let compwins = 0;
+
 const choices = ["Rock", "Paper", "Scissors"];
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener("click", game) //playRound
+});
+
+const resdiv = document.querySelector(".results");
+
+function addRound(outcome) {
+    const round = document.createElement("p");
+    round.textContent = outcome;
+    resdiv.appendChild(round);
+}
+
+function updateScore() {
+    document.getElementById("score").innerHTML = `Score: ${wins} : ${compwins}`;
+}
+
+function endGame() {
+    let winner;
+    buttons.forEach((button) => button.classList.add("disabled"))
+    if (wins > compwins) {
+        winner = "You";
+    } else {
+        winner = "Computer";
+    }
+    const endtext = document.createElement("p");
+    console.log(winner);
+    endtext.textContent = `${winner} won the game!`;
+    resdiv.append(endtext);
+}
 
 function computerPlay() {
     return choices[Math.floor(Math.random() * 3)];
@@ -15,56 +50,29 @@ function winLose(player, computer) {
     }
 }
 
-function cleanUserInput(playerSelection) {
-    return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-}
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = cleanUserInput(playerSelection);
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
     let outcome = winLose(playerSelection, computerSelection);
     if(outcome === "draw") {
-        console.log(`${playerSelection} draws ${computerSelection}. No winners!`);
+        addRound(`${playerSelection} draws ${computerSelection}. No winners!`);
         return "draw";
     } else if (outcome === "win") {
-        console.log(`${playerSelection} beats ${computerSelection}. You win!`);
+        addRound(`${playerSelection} beats ${computerSelection}. You win!`);
+        wins+=1;
         return "win";
     } else {
-        console.log(`${playerSelection} loses to ${computerSelection}. You lose!`);
+        addRound(`${playerSelection} loses to ${computerSelection}. You lose!`);
+        compwins+=1;
         return "lose";
     }
 }
 
-function game() {
-    let wins = 0;
-    let losses = 0;
-    let round;
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = computerPlay();
-        let playerSelection = window.prompt("Please enter rock, paper or scissors");
-        round = playRound(playerSelection, computerSelection);
-        if (round === "win") {
-            wins++;
-        } else if (round === "lose") {
-            losses++;
-        }
-    }
-
-    console.log("Wins: " + wins + " Losses: " + losses);
-
-    if (wins > losses) {
-        console.log(`You won with ${wins} wins and ${losses} losses.`);
-    } else if (losses > wins) {
-        console.log(`You lost with ${wins} wins and ${losses} losses.`);
-    } else if (wins === losses) {
-        console.log(`You drew with ${wins} wins and losses.`);
+function game(e) {
+    playerSelection = this.id;
+    playRound(playerSelection);
+    updateScore();
+    if (wins >= 5 || compwins >= 5) {
+        endGame();
     }
 }
-
-
-/*const playerSelection = "Rock";
-console.log(playerSelection);
-const computerSelection = computerPlay();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));*/
-
-game();
